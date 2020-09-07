a9326924913ac7f9ebd262a61d5e8fc9942b1d2b
fillooow <fil201121@yandex.ru>
2020-09-07 19:03:16 +0500

    переименования, оптимизация, добавил вычисление расстояния между итемами

--- app/src/main/java/fillooow/app/customprogressbar/custom_view/LinearSpeedometerProgressView.kt
+++ app/src/main/java/fillooow/app/customprogressbar/custom_view/LinearSpeedometerProgressView.kt
@@ -21,15 +21,14 @@ class LinearSpeedometerProgressView @JvmOverloads constructor(
     override val foregroundPaintColorResId: Int = R.color.kit_brand
 
     private val regularItemVerticalOffsetPx = asPixels(R.dimen.linear_speedometer_progress_bar_regular_item_vertical_offset)
-    private val itemsHorizontalOffsetPx = asPixels(R.dimen.linear_speedometer_progress_bar_item_horizontal_offset)
 
     private val regularItemRadiusPx = asPixels(R.dimen.linear_speedometer_progress_bar_item_radius)
 
     private val regularItemHeightPx = asPixels(R.dimen.linear_speedometer_progress_bar_regular_item_height)
-    private val regularItemWidthPx = asPixels(R.dimen.linear_speedometer_progress_bar_regular_item_width)
-
     private val bigItemHeightPx = asPixels(R.dimen.linear_speedometer_progress_bar_big_item_height)
-    private val bigItemWidthPx = asPixels(R.dimen.linear_speedometer_progress_bar_big_item_width)
+    private val itemWidthPx = asPixels(R.dimen.linear_speedometer_progress_bar_item_width)
+
+    private var horizontalItemsOffset = 0f
 
     /**
      * Так как [bigItem] выше, чем [regularItem], нужно добавить отступ сверху для [regularItem],
@@ -37,8 +36,8 @@ class LinearSpeedometerProgressView @JvmOverloads constructor(
      */
     private val regularItemTopOffsetCalculated = (bigItemHeightPx - regularItemHeightPx) / 2
 
-    private val regularItem = RectF(0f, regularItemVerticalOffsetPx, regularItemWidthPx, regularItemHeightPx + regularItemTopOffsetCalculated)
-    private val bigItem = RectF(0f, 0f, bigItemWidthPx, bigItemHeightPx)
+    private val regularItem = RectF(0f, regularItemVerticalOffsetPx, itemWidthPx, regularItemHeightPx + regularItemTopOffsetCalculated)
+    private val bigItem = RectF(0f, 0f, itemWidthPx, bigItemHeightPx)
 
     private val rectPaint = Paint().apply {
 
@@ -47,20 +46,36 @@ class LinearSpeedometerProgressView @JvmOverloads constructor(
         isAntiAlias = true
     }
 
+    /**
+     * Всего 27 элементов.
+     *
+     * Из них:
+     * [regularItem] - 24,
+     * [bigItem] - 3.
+     */
+    private val itemsRange = 1 .. 27
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        horizontalItemsOffset = calculateItemsOffset()
+    }
+
     override fun Canvas.drawProgress() {
 
         save()
-        for (i in 1 .. 27) {
+        for (itemPosition in itemsRange) {
 
-            rectPaint.color = when (i <= animatedProgress.toInt()) {
+            rectPaint.color = when (itemPosition <= animatedProgress.toInt()) {
 
                 true -> foregroundPaint.color
                 false -> backgroundPaint.color
             }
 
-            if (i.rem(7) != 0) drawRegularItem() else drawBigItem()
-            translate(itemsHorizontalOffsetPx, 0f)
+            if (itemPosition.rem(7) != 0) drawRegularItem() else drawBigItem()
+            translate(horizontalItemsOffset + itemWidthPx, 0f)
         }
+//        drawLine(0f, bigItemHeightPx / 2 , measuredWidth.toFloat(), bigItemHeightPx / 2, rectPaint.apply { color = getColor(R.color.colorAccent)})
         restore()
     }
 
@@ -69,4 +84,27 @@ class LinearSpeedometerProgressView @JvmOverloads constructor(
     private fun Canvas.drawBigItem() = drawRoundRect(bigItem, regularItemRadiusPx, regularItemRadiusPx, rectPaint)
 
     private fun asPixels(@DimenRes dimensionResource: Int) = resources.getDimension(dimensionResource)
+
+    /**
+     * Считает расстояние между item-ами в зависимости от ширины экрана
+     * доступной для отрисовки [LinearSpeedometerProgressView].
+     * Опирается на [getMeasuredWidth]
+     */
+    private fun calculateItemsOffset(): Float = (measuredWidth - itemWidthPx * itemsRange.last) / (itemsRange.last - 1)
 }
+
+
+
+/*
+private class BigItem : Item() {
+
+    override fun drawItem() {
+        TODO("Not yet implemented")
+    }
+}
+
+private abstract class Item() {
+
+    abstract fun drawItem()
+}
+*/
