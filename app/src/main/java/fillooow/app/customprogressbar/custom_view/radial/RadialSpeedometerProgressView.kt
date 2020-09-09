6d7ff1a6b91c1b350827463a1f43a7cf352b8dd7
fillooow <fil201121@yandex.ru>
2020-09-09 14:44:35 +0500

    добавил радиальный спидометр в разметку, накинул строковых ресурсов, засел за работу

--- app/src/main/java/fillooow/app/customprogressbar/custom_view/radial/RadialSpeedometerProgressView.kt
+++ app/src/main/java/fillooow/app/customprogressbar/custom_view/radial/RadialSpeedometerProgressView.kt
@@ -2,7 +2,11 @@ package fillooow.app.customprogressbar.custom_view.radial
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.util.AttributeSet
+import androidx.annotation.DimenRes
+import androidx.core.view.updateLayoutParams
+import fillooow.app.customprogressbar.R
 import fillooow.app.customprogressbar.custom_view.base.BaseProgressView
 
 class RadialSpeedometerProgressView @JvmOverloads constructor(
@@ -20,7 +24,21 @@ class RadialSpeedometerProgressView @JvmOverloads constructor(
     override val foregroundPaintColorResId: Int
         get() = TODO("Not yet implemented")
 
+    private val viewWidth = resources.getDimensionPixelSize(R.dimen.radial_speedometer_progress_bar_stroke_width)
+    private val viewHeight = resources.getDimensionPixelSize(R.dimen.radial_speedometer_progress_bar_stroke_height)
+
     override fun Canvas.drawProgress() {
         TODO("Not yet implemented")
     }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        updateLayoutParams {
+            width = viewWidth
+            height = viewHeight
+            setBackgroundColor(Color.CYAN)
+        }
+    }
+
+    private fun asPixels(@DimenRes dimensionResource: Int) = resources.getDimension(dimensionResource)
 }
