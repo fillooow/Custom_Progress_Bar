43dd7a55d5c61c88284013933dff900a5c784714
fillooow <fil201121@yandex.ru>
2020-09-15 15:05:18 +0500

    подтянул датабиндинги

--- app/src/main/java/fillooow/app/customprogressbar/LinearSpeedometerActivity.kt
+++ app/src/main/java/fillooow/app/customprogressbar/LinearSpeedometerActivity.kt
@@ -1,11 +1,21 @@
 package fillooow.app.customprogressbar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import fillooow.app.customprogressbar.databinding.ActivityLinearSpeedometerBinding
 
 class LinearSpeedometerActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<LinearSpeedometerViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_linear_speedometer)
+
+        val binding: ActivityLinearSpeedometerBinding = DataBindingUtil.setContentView(this, R.layout.activity_linear_speedometer)
+
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
     }
 }
--- app/src/main/java/fillooow/app/customprogressbar/RadialSpeedometerActivity.kt
+++ app/src/main/java/fillooow/app/customprogressbar/RadialSpeedometerActivity.kt
@@ -1,11 +1,21 @@
 package fillooow.app.customprogressbar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import fillooow.app.customprogressbar.databinding.ActivityRadialSpeedometerBinding
 
 class RadialSpeedometerActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<RadialSpeedometerViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_radial_speedometer)
+
+        val binding: ActivityRadialSpeedometerBinding = DataBindingUtil.setContentView(this, R.layout.activity_radial_speedometer)
+
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
     }
 }
